ab1230b7d496d348a5cad3a830035e5abbe856e0
shannonklett <shannonklett@users.noreply.github.com>
2014-10-09 16:06:24 -0400

    Code clean up
    (Organized methods)

--- front_end.py
+++ front_end.py
@@ -1,4 +1,6 @@
-#global prompts
+'''
+GLOBAL PROMPTS
+'''
 prompt_login = "Enter 'login' to begin:"
 prompt_retail_agent = "Are you 'retail' or 'agent'?"
 prompt_command = "Enter a valid command:"
@@ -13,7 +15,9 @@ prompt_transfer_to = "Enter a valid account number to transfer to:"
 prompt_transfer = "Enter transfer amount:"
 prompt_finish = "If you would like to shut down, type 'Quit'"
 
-#global errors
+'''
+GLOBAL ERROR PROMPTS
+'''
 error_account_num = "Error: Account numbers must be 1-6 digits."
 error_account_nam = "Error: Account names must be 1-15 characters."
 error_retail_amount = "Error: Transactions above $1,000.00 are not accepted in 'retail' mode."
@@ -25,16 +29,44 @@ error_account_exists = "Error: Account already exists."
 error_account_neg = "Error: Account number must be greater than 0."
 error_transfer_same ="Error: Cannot transfer to the same account."
 
+'''
+GLOBAL VARIABLES
+'''
 Loggedin = False
 Agent = False
 Running = True
 temp_transaction_summary = []
 
-def inAccountFile(num):
-	#check if account number is in master file
-	return False
+'''
+READ/WRITE FILE FUNCTIONS
+'''
+def readAccountFile(filename):
+	lines = [line.strip() for line in open(filename)]
+	return lines
 
-def checkAccountNum(num):
+def writeTransactionFile(transactions):
+	f=open('Testing/Temp/output.txt', 'w+')
+	for item in transactions:
+		f.write("%s\n" % item)
+	f.write("%s" % makeTransactionString())		
+	f.close()
+	
+def makeTransactionString(type=00, account1=000000, account2=000000, amount=00000000, name=""):
+	type = str(type).rjust(2, "0")
+	account1 = str(account1).rjust(6, "0")
+	account2 = str(account2).rjust(6, "0")
+	amount = str(amount).rjust(8, "0")
+	name = name.ljust(15)
+	return "%s %s %s %s %s" %(type, account1, account2, amount, name)	
+
+'''
+INPUT CHECKING METHODS
+'''
+def checkAccountExists(num):
+	num = str(num).rjust(6, "0")
+	return num in account_list
+
+def checkAccountNum(num, createMode=False):
 	try:
 		num = int(num)
 	except ValueError:
@@ -44,7 +76,9 @@ def checkAccountNum(num):
 		print error_account_num
 	elif num<1:
 		print error_account_neg
-	elif inAccountFile(num):
+	elif not createMode and not checkAccountExists(num):
+		print error_account_dne		
+	elif createMode and checkAccountExists(num):
 		print error_account_exists
 	else:
 		return True
@@ -55,31 +89,36 @@ def checkAccountName(name):
 		print error_account_nam
 	else:
 		return True
-	return False #error occured	
+	return False #error occured
 
-def makeTransactionString(type=00, account1=000000, account2=000000, amount=00000000, name=""):
-	type = str(type).rjust(2, "0")
-	account1 = str(account1).rjust(6, "0")
-	account2 = str(account2).rjust(6, "0")
-	amount = str(amount).rjust(8, "0")
-	name = name.ljust(15)
-	return "%s %s %s %s %s" %(type, account1, account2, amount, name)
+def checkAmount(val):
+	if Agent:
+		if int(val) > 99999999:
+			print error_agent_amount
+			return False
+		elif int(val) < 0:
+			print error_amount_type
+			return False
+		else:
+			return True
+	else:	
+		if int(val) > 100000:
+			print error_retail_amount
+			return False
+		elif int(val) < 0:
+			print error_amount_type
+			return False
+		else:
+			return True	
 
-def readAccountFile(filename):
-	lines = [line.strip() for line in open(filename)]
-	return lines
-
-def writeTransactionFile(transactions):
-	f=open('Testing/transactionFile.txt', 'w+')
-	for item in transactions:
-		f.write("%s\n" % item)
-	f.write("%s" % makeTransactionString())		
-	
+'''
+ACCOUNT CHANGING FUNCTIONS
+'''
 def create():
 	if Agent:
 		print prompt_new_account_num
 		account_num = raw_input()
-		if checkAccountNum(account_num):
+		if checkAccountNum(account_num, True):
 			print prompt_new_account_nam
 			account_name = raw_input()
 			if checkAccountName(account_name):
@@ -101,26 +140,9 @@ def delete():
 		print error_permission
 	return None #error occured	
 
-def val_check(val):
-	if Agent:
-		if int(val) > 99999999:
-			print error_agent_amount
-			return False
-		elif int(val) < 0:
-			print error_amount_type
-			return False
-		else:
-			return True
-	else:	
-		if int(val) > 100000:
-			print error_retail_amount
-			return False
-		elif int(val) < 0:
-			print error_amount_type
-			return False
-		else:
-			return True
-	
+'''
+TRANSACTION FUNCTIONS
+'''
 def deposit():	
 	print prompt_valid_account_num
 	account_num = raw_input()
@@ -133,7 +155,7 @@ def deposit():
 	except ValueError:
 		print error_amount_type
 		return None
-	if(val_check(deposit_val)):
+	if(checkAmount(deposit_val)):
 		print "Deposit Successful"
 		return makeTransactionString(1, account_num, amount = deposit_val)
 	else:
@@ -152,7 +174,7 @@ def withdraw():
 	except ValueError:
 		print error_amount_type
 		return None
-	if (val_check(withdraw_val)):
+	if (checkAmount(withdraw_val)):
 		print "Withdraw Successful"
 		return makeTransactionString(2, account2 = account_num, amount = withdraw_val)
 	else:
@@ -175,21 +197,23 @@ def transfer():
 	except ValueError:
 		print error_amount_type
 		return None
-	if (val_check(transfer_val)):
+	if (checkAmount(transfer_val)):
 		print "Transfer Successful"
 		return makeTransactionString(3, account_num_to, account_num_from, transfer_val)
 	else:
 		return None
 	return None
 
-#Main Execution
-accounts = readAccountFile('Testing/Inputs/accountList_1_2.txt')
-print accounts
+'''
+MAIN PROGRAM
+'''
 while (Running):
 	while (Loggedin == False):
 		print prompt_login
 		log = raw_input()
 		while (log == 'Login' or log == 'login'):
+			#read in valid accounts file
+			account_list = readAccountFile('Testing/Inputs/accountList_1_2.txt')
 			print prompt_retail_agent
 			user_type = raw_input()
 			if (user_type == 'Agent' or user_type == 'agent'):
@@ -227,12 +251,9 @@ while (Running):
 				temp_transaction_summary.append(summary)
 		elif(com == 'Logout' or com == 'logout'):
 			Loggedin = False
+			#write Transaction Summary File
 			writeTransactionFile(temp_transaction_summary)
-		else:
-			None
 	print prompt_finish
 	quit = raw_input()
 	if (quit == 'Quit' or quit == 'quit'):
 		Running = False
-	else:
-		None
