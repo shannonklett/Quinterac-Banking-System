73e9bba997b1ded64679f8fbe815d203c28c0eed
shannonklett <shannon.klett@gmail.com>
2014-10-30 09:58:07 -0400

    Fixing from 10:29-15:07:06

--- Testing/masterScript.sh
+++ Testing/masterScript.sh
@@ -1,14 +1,22 @@
-DATE=$(date +'%m-%d:%H:%M:%S')
+DATE=$(date +'%m-%d:%H:%M:%S') #used to differentiate each test run
 mkdir TestOutputs/$DATE
 mkdir TestPrompts/$DATE
 
 while read test; do
+	#run test
 	printf "running test $test\n"
+	#Take account list from ValidAccounts (000001, 000002, etc
+	#Output transaction summary to TestOutputs (01 0000001..., etc)
+	#Read commands from Auto_Inputs (login, agent, etc.)
+	#Output prompts to TestPrompts (Enter 'login', etc.) 
 	python ../front_end_modified.py ValidAccounts/$test TestOutputs/$DATE/$test < Auto_Inputs/$test > TestPrompts/$DATE/$test
 	
-	printf "\nchecking output of test $test\n"
+	#check against expected output file and prompts
+	printf "checking output of test $test\n"
 	diff -b TestOutputs/$DATE/$test ExpectedOutputs/$test
 	diff -b TestPrompts/$DATE/$test ExpectedPrompts/$test
-done <inputFiles.txt
+	printf "\n\n"
+
+done <inputFiles.txt #list of all test cases in proper order
 
 
